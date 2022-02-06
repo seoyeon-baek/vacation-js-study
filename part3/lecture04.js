a7238273bba89992f4757b6151f77d51c2492338
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-02-07 02:37:39 +0900

    기본자료형
    number, string, boolean

---
+++ part3/lecture04.js
@@ -0,0 +1,9 @@
+var a = 100, b = 3.14; //number
+var c = "안녕하세요", d="a"; //string
+var e = true, f = false; //boolean
+
+// typeof() 란?
+// 괄호 안에 들어있는 변수의 type명을 알려주는 명령어
+console.log(a,typeof(a));
+console.log(c,typeof(c));
+console.log(e,typeof(e));
